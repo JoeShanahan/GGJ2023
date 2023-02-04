ec95f1b3ba38bafe01c6bd26631982bbe518bbcd
JoeLaptop <iam3Djoe@gmail.com>
2023-02-04 19:18:10 +0000

    first pass of worker allocate UI

---
+++ Assets/Scripts/UI/WorldToCanvas/WorkerAllocateUI.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class WorkerAllocateUI : W2C
+{
+    [SerializeField]
+    private TMP_Text _numberText;
+
+    [SerializeField]
+    private RectTransform _plusButton;
+
+    [SerializeField]
+    private RectTransform _minusButton;
+
+    [SerializeField]
+    private Resource _resourceType;
+
+    public void ButtonPressPlus()
+    {
+
+    }
+
+    public void ButtonPressMinus()
+    {
+
+    }
+}
