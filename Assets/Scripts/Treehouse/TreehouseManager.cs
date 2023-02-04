efa9e9cf70702fc35db18e5b750697a110f5c703
JoeLaptop <iam3Djoe@gmail.com>
2023-02-04 11:58:00 +0000

    made it possible to grow treehouse

--- Assets/Scripts/Treehouse/TreehouseManager.cs
+++ Assets/Scripts/Treehouse/TreehouseManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using EasyButtons;
 
 public class TreehouseManager : MonoBehaviour
 {
@@ -10,10 +11,45 @@ public class TreehouseManager : MonoBehaviour
     [SerializeField]
     private List<TreehouseRoom> _rooms;
 
+    [Header("Background Graphics")]
+    [SerializeField]
+    private SpriteRenderer _stumpGraphic;
+
+    [SerializeField]
+    private SpriteRenderer[] _floorGraphics;
+
+    [SerializeField]
+    private SpriteRenderer[] _rootsGraphics;
+
+    [SerializeField]
+    private SpriteRenderer[] _branchesGraphics;
+
+    [Button]
+    public void IncreaseTreeHeight()
+    {
+        int maxHeight = _floorGraphics.Length;
+
+        if (_currentHeight >= maxHeight)
+            return;
+
+        _currentHeight ++;
+
+        _floorGraphics[_currentHeight-1].gameObject.SetActive(true);
+        _rooms[_currentHeight-1].gameObject.SetActive(true);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach (SpriteRenderer graphic in _floorGraphics)
+        {
+            graphic.gameObject.SetActive(false);
+        }
+
+        foreach (TreehouseRoom room in _rooms)
+        {
+            room.gameObject.SetActive(false);
+        }
     }
 
     // Update is called once per frame
