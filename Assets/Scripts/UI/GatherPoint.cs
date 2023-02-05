41fc902f22f48cde7d63735b93b9716709c4d9f0
Luke Stampfli <luke@stampfli.one>
2023-02-05 11:10:26 +0000

    add: hide worke allocation UI until correct stage unlocked

--- Assets/Scripts/UI/GatherPoint.cs
+++ Assets/Scripts/UI/GatherPoint.cs
@@ -2,9 +2,10 @@ using System.Collections.Generic;
 using EasyButtons;
 using UnityEngine;
 
-
+[DefaultExecutionOrder(1000)] // after ProgressionManager
 public class GatherPoint : MonoBehaviour
 {
+    public ProgressStep Step;
     public Resource resourceType;
     public Transform gatherPoint => transform;
     public Transform dropOffPoint;
@@ -14,10 +15,31 @@ public class GatherPoint : MonoBehaviour
     [SerializeField]
     private GameObject _uiPrefab;
 
-    void Start()
+    void SetupUI()
     {
         _allocateUI = W2C.InstantiateAs<WorkerAllocateUI>(_uiPrefab);
         _allocateUI.Setup(resourceType);
         _allocateUI.SetPosition(gatherPoint);
+    }
+    
+    void Start()
+    {
+        if (Step == ProgressStep.Unknown)
+        {
+            SetupUI();
+        }
+        else
+        {
+            ProgressionManager.Subscribe(OnProgressionStepComplete);
+        }
+    }
+    
+    private void OnProgressionStepComplete()
+    {
+        if (ProgressionManager.HasDone(Step))
+        {
+            SetupUI();
+            ProgressionManager.Unsubscribe(OnProgressionStepComplete);
+        }
     }
 }
